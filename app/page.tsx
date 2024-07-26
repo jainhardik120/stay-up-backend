66ecc3ac1a0e2e2ba813b83a3ae441eab8e094ac
Hardik Jain <jainhardik120@gmail.com>
2024-07-26 11:23:35 +0530

    fixed Client component error

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 import Image from "next/image";
 import NewSlideForm from "./components/NewSlideForm";
+import NotificationForm from "./components/NotificationForm";
 
 export default function Home() {
   return (
-    <div>
+    <div className="flex flex-col items-center justify-center gap-4 p-4">
+      <h1>Stay Up Admin Panel</h1>
       <NewSlideForm />
+      <NotificationForm />
     </div>
   );
 }
